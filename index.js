33e56ac86e1459cdbee80d17f7705c1097cdc2a3
Gergo Buglyo <buglyogergo@gmail.com>
2023-04-06 21:57:13 +0200

    Created routes list

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
+require("dotenv").config();
+
 const http = require("http");
 const fs = require("fs");
-require("dotenv").config();
 
 const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-  res.end("Server");
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname;
+  const query = url.searchParams;
+  const match = url.pathname.match(/^\/api\/products\/(\d+)$/);
+  const id = match ? match[1] : null;
+
+  //console.log(url);
+  console.log(id);
+  console.log(pathname);
+  console.log(query.get("item"));
+
+  // Creating routes
+  switch (true) {
+    //TODO: /api/products -> GET all items
+    case pathname === "/api/products" && req.method === "GET":
+      res.end("GET Products list");
+      break;
+
+    //TODO: /api/products -> POST add new item to the list
+    case pathname === "/api/products" && req.method === "POST":
+      res.end("POST a new product");
+      break;
+
+    //TODO: /api/products/{id} -> GET a item from the list by id
+    case id && req.method === "GET":
+      res.end("GET product by id");
+      break;
+
+    //TODO: /api/products/{id} -> PATCH a item from the list by id
+    case id && req.method === "PATCH":
+      res.end("EDIT product by id");
+      break;
+
+    //TODO: /api/products/{id} -> DELETE a item from the list by id
+    case id && req.method === "DELETE":
+      res.end("DELETE product by id");
+      break;
+
+    //TODO: Get default response
+    default:
+      res.writeHead(404);
+      res.end("Page not found");
+  }
 });
 
 server.listen(port, () => console.log(`Server is running on ${port}`));
